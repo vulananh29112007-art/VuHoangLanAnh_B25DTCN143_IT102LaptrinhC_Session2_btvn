f8898fc50758875ddf684d48a3b835985ad869ef
vulananh29112007-art <vulananh29112007@gmail.com>
2025-10-18 15:23:45 +0700

    bvnBTTHss2.c

--- bvnBTTHss2.c
+++ bvnBTTHss2.c
@@ -3,10 +3,10 @@
 int main(){
 	
 	int a=9,b=7,c=8;
-	float S;
-	S= (pow(a,2)+sqrt(pow(b,2)+4*a*c))/(2*a)-(pow(b,3)/pow(c,2))+sqrt(fabs(a-b));
+	float S = (pow(a,2)+sqrt(pow(b,2)+4*a*c))/(2*a)-(pow(b,3)/pow(c,2))+sqrt(fabs(a-b));
 	printf("%f",S); 
 	
 	return 0; 
 } 
+
 
